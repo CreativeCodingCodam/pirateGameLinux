ac6ace9f824f86d2446a2d42aa77d50f06bc8ec4
sebastianpenning <whimizera@mailbox.org>
2025-06-16 16:16:20 +0200

    added getters in Colour Class

--- src/Colour.cpp
+++ src/Colour.cpp
@@ -1,5 +1,25 @@
 #include "Colour.h"
 
+int Colour::getRed()
+{
+    return (r);
+}
+
+int Colour::getGreen()
+{
+    return (g);
+}
+
+int Colour::getBlue()
+{
+    return (b);
+}
+
+int Colour::getAlpha()
+{
+    return (a);
+}
+
 Colour::Colour(int r, int g, int b, float a)
 : r(r), g(g), b(b), a(a)
 {
--- src/Colour.h
+++ src/Colour.h
@@ -3,9 +3,13 @@
 
 class Colour {
 public:
+    int getRed();
+    int getGreen();
+    int getBlue();
+    int getAlpha();
+//  implement interpolateColours();
     Colour(int r, int g, int b, float a);
     ~Colour();
-
 private:
     int r;
     int g;
