2345f0a69eba045823efc8d8d7640cfd95e99480
sebastianpenning <whimizera@mailbox.org>
2025-06-18 11:27:49 +0200

    added lightColour to vector conversion

--- src/TerrainRenderer.cpp
+++ src/TerrainRenderer.cpp
@@ -1,5 +1,7 @@
 #include "TerrainRenderer.h"
+#include "fwd.hpp"
 #include "ofConstants.h"
+#include "ofVec3f.h"
 
 
 TerrainRenderer::TerrainRenderer(TerrainShader shader, bool usesIndices)
@@ -19,11 +21,12 @@ void TerrainRenderer::render(Terrain terrain, ofCamera camera, ofLight light)
 
 void TerrainRenderer::prepare(Terrain terrain, ofCamera camera, ofLight light)
 {
+    int hex = light.getDiffuseColor().getHex();
+    glm::vec3 lightColour(((hex >> 16) & 0xFF), ((hex >> 8) & 0xFF), (hex & 0xFF));
     terrain.getVbo().bind();
     shader.begin();
     shader.setUniform3f("lightDirection", light.getLookAtDir());
-    // TODO figure how to create vector from color float
-    shader.setUniform3f("lightColour", light.getDiffuseColor());
+    shader.setUniform3f("lightColour", lightColour);
     shader.setUniform2f("lightBias", glm::vec2(0.5, 0.5));
     shader.setUniformMatrix4f("projectionViewMatrix", camera.getModelViewProjectionMatrix());
 }
