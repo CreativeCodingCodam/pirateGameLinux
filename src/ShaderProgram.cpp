e993d8da8b3b4ee4490fee41fd941a7ed6aed8c6
sebastianpenning <whimizera@mailbox.org>
2025-06-19 12:54:55 +0200

    added setting function in ShaderProgram

--- src/ShaderProgram.cpp
+++ src/ShaderProgram.cpp
@@ -12,6 +12,21 @@ void ShaderProgram::end(void)
     shader.end();
 }
 
+void ShaderProgram::setUniformMatrix4f(const std::string &name, glm::mat4 m)
+{
+    shader.setUniformMatrix4f(name, m, 1);
+}
+
+void ShaderProgram::setUniform3f(const std::string &name, glm::vec3 v)
+{
+    shader.setUniform3f(name, v);
+}
+
+void ShaderProgram::setUniform2f(const std::string &name, glm::vec2 v)
+{
+    shader.setUniform2f(name, v);
+}
+
 ShaderProgram::ShaderProgram(std::filesystem::path vertexFile, std::filesystem::path fragmentFile)
 {
     shader.load(vertexFile, fragmentFile);
